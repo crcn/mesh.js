22f1df50d8fb9c21030b55cf8286ecd51044ebc5
Craig Condon <craig.condon@hellosign.com>
2015-03-27 21:17:01 -0700

    add delta

---
+++ test/delta-test.js
@@ -0,0 +1,36 @@
+var crudlet = require("../");
+var expect  = require("expect.js");
+var through = require("through2");
+
+describe(__filename + "#", function() {
+  it("can pipe only changed data", function(next) {
+
+    var db1 = function() {
+      return through.obj(function(operation, enc, next) {
+        this.push(operation.data);
+        next();
+      });
+    }
+
+    var db = crudlet(db1);
+    var stream = db();
+
+
+    var c = stream.pipe(crudlet.delta());
+    c.once("data", function(data) {
+      expect(data.name).to.be("abba");
+      c.once("data", function(data) {
+        expect(data.name).to.be(void 0);
+        expect(data.age).to.be(19);
+        c.once("end", function() {
+          next();
+        });
+        stream.end();
+      });
+      stream.write(crudlet.operation("update", { data: { age: 19 }}));
+    });
+
+    stream.write(crudlet.operation("update", { data: { name: "abba" }}));
+
+  });
+});
