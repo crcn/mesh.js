d485b7987352a76c2d8c05a29d80bb7b613b0bf1
Craig Condon <craig.condon@hellosign.com>
2015-04-01 22:34:52 -0700

    pipeline test

---
+++ test/recipes-test.js
@@ -0,0 +1,27 @@
+var crudlet = require("../");
+var expect  = require("expect.js");
+var through = require("through2");
+var _       = require("highland");
+
+describe(__filename + "#", function() {
+
+  it("can map data to another value", function(next) {
+
+    function db(name, props) {
+      return _([{ name: "a" }, { name: "b" }]);
+    }
+
+    db().
+    pipe(_.pipeline(
+      _.map(function(d) { return d.name; }),
+      _.collect
+    )).
+    on("data", function(items) {
+      expect(items[0]).to.be("a");
+      expect(items[1]).to.be("b");
+      next();
+    });
+  });
+
+
+});
