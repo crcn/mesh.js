3276e12bdb917c2677fa60d034d45f6bf5cfa03b
Craig Condon <craig.condon@hellosign.com>
2015-04-07 14:36:37 -0700

    cleanup - no more process.nextTick

--- test/wrap-callback-test.js
+++ test/wrap-callback-test.js
@@ -25,4 +25,17 @@ describe(__filename + "#", function() {
       next();
     });
   });
+
+  it("emits multiple values if the returned value is an array", function(next) {
+    var db = mesh.wrapCallback(function(operation, next) {
+      next(void 0, [1, 2]);
+    });
+
+    db(mesh.op("insert")).pipe(_.pipeline(_.collect)).on("data", function(data) {
+      expect(data.length).to.be(2);
+      expect(data[0]).to.be(1);
+      expect(data[1]).to.be(2);
+      next();
+    });
+  });
 });
