ced462d723608fb14644c7916de6e66f85ce84f6
Craig Condon <craig.condon@hellosign.com>
2015-04-16 14:41:46 -0700

    add tests

--- test/attach-test.js
+++ test/attach-test.js
@@ -22,31 +22,45 @@ describe(__filename + "#", function() {
 
   it("copies configs over from a previously decorated bus", function() {
 
-      function bus(operation) {
-        return _([operation]);
-      }
+    function bus(operation) {
+      return _([operation]);
+    }
 
-      var child  = mesh.attach({ a: 1 }, bus);
-      expect(child.options.a).to.be(1);
-      expect(child.target).to.be(bus);
-      child = mesh.attach({ b: 1 }, child);
-      expect(child.options.a).to.be(1);
-      expect(child.options.b).to.be(1);
-      expect(child.target).to.be(bus);
-      // expect(child.)
+    var child  = mesh.attach({ a: 1 }, bus);
+    expect(child.options.a).to.be(1);
+    expect(child.target).to.be(bus);
+    child = mesh.attach({ b: 1 }, child);
+    expect(child.options.a).to.be(1);
+    expect(child.options.b).to.be(1);
+    expect(child.target).to.be(bus);
+    // expect(child.)
   });
 
   it("can override a previously decorated prop", function() {
 
-      function bus(operation) {
-        return _([operation]);
-      }
+    function bus(operation) {
+      return _([operation]);
+    }
 
-      var child  = mesh.attach({ a: 1 }, bus);
-      expect(child.options.a).to.be(1);
-      expect(child.target).to.be(bus);
-      child = mesh.attach({ a: 2 }, child);
-      expect(child.options.a).to.be(2);
-      expect(child.target).to.be(bus);
+    var child  = mesh.attach({ a: 1 }, bus);
+    expect(child.options.a).to.be(1);
+    expect(child.target).to.be(bus);
+    child = mesh.attach({ a: 2 }, child);
+    expect(child.options.a).to.be(2);
+    expect(child.target).to.be(bus);
+  });
+
+  it("doesn't set props on the original operation", function(next) {
+
+    var bus = mesh.wrap(function(operation, next) {
+      next(void 0, 1);
+    });
+
+    bus = mesh.attach({ a: 1 }, bus);
+    var op  = mesh.op("load");
+    bus(op).on("end", function() {
+      expect(op.a).to.be(void 0);
+      next();
+    });
   });
 });
