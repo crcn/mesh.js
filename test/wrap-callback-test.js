48f2afc4bbb9b985f336b126ac03a23632de0bf2
Craig Condon <craig.condon@hellosign.com>
2015-06-19 18:02:39 -0700

    do not handle multi flag

--- test/wrap-callback-test.js
+++ test/wrap-callback-test.js
@@ -26,18 +26,6 @@ describe(__filename + "#", function() {
     });
   });
 
-  it("emits one value if the returned value is an array", function(next) {
-    var bus = mesh.wrap(function(operation, next) {
-      next(void 0, [1, 2]);
-    });
-
-    bus(mesh.op("insert")).pipe(_.pipeline(_.collect)).on("data", function(data) {
-      expect(data.length).to.be(1);
-      expect(data[0]).to.be(1);
-      next();
-    });
-  });
-
   it("emits multiple values if the returned value is an array and multi=true", function(next) {
     var bus = mesh.wrap(function(operation, next) {
       next(void 0, [1, 2]);
@@ -49,14 +37,5 @@ describe(__filename + "#", function() {
       expect(data[1]).to.be(2);
       next();
     });
-  });
-
-  // don't do this
-  xit("ends the stream after an error", function(next) {
-    var bus = mesh.wrap(function(op, next) {
-      next(new Error("err"));
-    });
-
-    bus(mesh.op("aa")).on("error", function() { }).on("end", next);
   });
 });
