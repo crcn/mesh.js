a2cbbcd1c664c8a785020602a7f184edd182007d
Craig Condon <craig.j.condon@gmail.com>
2016-10-04 17:45:33 -0500

    update typescript definition

--- packages/mesh-socket-io-bus/index.d.ts
+++ packages/mesh-socket-io-bus/index.d.ts
@@ -2,12 +2,18 @@
 declare module "mesh-socket-io-bus" {
   import { Bus } from 'mesh';
 
+  interface SocketIOOptions {
+    channel?: string;
+    client?: any;
+  }
+
   interface SocketIOConnection {
     on(eventType:string, listener:Function);
     emit(eventType:string, ...args:Array<any>);
   }
 
   class SocketIOBus extends Bus {
+    constructor(options?: SocketIOOptions);
     static create({ connection:SocketIOConnection }, publicBus:Bus):SocketIOBus;
   }
 
