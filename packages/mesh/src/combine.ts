432dc91b824934dbf49afd77baecad2e98c17d71
Craig Condon <craig.j.condon@gmail.com>
2017-07-10 09:44:00 -0400

    remove return

--- packages/mesh/src/combine.ts
+++ packages/mesh/src/combine.ts
@@ -7,32 +7,31 @@ export type IteratorType<T> = (items: T[], each: (value: T) => any) => any;
 
 export const combine = <TInput, TOutput>(
   fns: Function[], 
-  iterator: IteratorType<Function>): ((...args: any[]) => AsyncIterableIterator<TOutput>) => {
-  return (...args: any[]) => createDuplexStream((input, output) => {
-    let primaryInput   = input as AsyncIterableIterator<any>;
+  iterator: IteratorType<Function>): ((...args: any[]) => AsyncIterableIterator<TOutput>) => (...args: any[]) => createDuplexStream((input, output) => {
+  let primaryInput   = input as AsyncIterableIterator<any>;
 
-    const inputs = Array.from({ length: fns.length }).map(v => {
-      let input;
-      [input, primaryInput] = tee(primaryInput);
-      return input;
-    });
+  const inputs = Array.from({ length: fns.length }).map(v => {
+    let input;
+    [input, primaryInput] = tee(primaryInput);
+    return input;
+  });
 
-    iterator(fns, call => {
-      const index = fns.indexOf(call);
-      const input = inputs[index];
-      const iter = wrapAsyncIterableIterator(call(...args));
-      const next = () => {
-        return input.next().then(({ value, done }) => {
-          return iter.next(value).then(({ value, done }) => {
-            if (done) {
-              return;
-            } else {
-              return output.unshift(value).then(next);
-            }
-          });
+  iterator(fns, call => {
+    const index = fns.indexOf(call);
+    const input = inputs[index];
+    const iter = wrapAsyncIterableIterator(call(...args));
+    const next = () => {
+      return input.next().then(({ value, done }) => {
+        return iter.next(value).then(({ value, done }) => {
+          if (done) {
+            return;
+          } else {
+            return output.unshift(value).then(next);
+          }
         });
-      };
-      return next();
-    }).then(() => output.done(), e => output.error(e));
-  })
-}
+      });
+    };
+    return next();
+  }).then(() => output.done(), e => output.error(e));
+});
+
