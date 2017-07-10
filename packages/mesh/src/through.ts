64b8f1388b30b9cbb1352cf55600940d33ac6f4c
Craig Condon <craig.j.condon@gmail.com>
2017-07-10 15:13:02 -0400

    simplify through

--- packages/mesh/src/through.ts
+++ packages/mesh/src/through.ts
@@ -1,5 +1,6 @@
 import { pump } from "./pump";
 import { createQueue } from "./queue";
+import { createDuplex } from "./duplex";
 import { wrapAsyncIterableIterator } from "./wrap-async-iterable-iterator";
 
 export function through<TInput, TOutput>(fn: (input: TInput) => TOutput, keepOpen?: boolean): AsyncIterableIterator<TOutput>;
@@ -8,49 +9,20 @@ export function through<TInput, TOutput>(fn: (input: TInput) => AsyncIterableIte
 
 // TODO - possibly endOnNoInput
 export function through<TInput>(fn: (input: TInput) => any, keepOpen: boolean = false) {
-  let _running: boolean;
-  let _pumping: boolean;
-  const outputQueue = createQueue();
-  const inputQueue  = createQueue<TInput>();
-
-  const run = () => {
-    if (_running) {
-      return;
-    }
-    _running = true;
-
+  return createDuplex<any, any>((input, output) => {
     const nextInput = () => {
-      inputQueue.next().then(({value}) => {
-        _pumping = true;
+      input.next().then(({value}) => {
+        if (value == null) {
+          if (!keepOpen) {
+            return output.return();
+          }
+        }
         return pump(wrapAsyncIterableIterator(fn(value)), (value) => {
-          return outputQueue.unshift(value);
-        }).then(() => {
-          _pumping = false;
-          nextInput();
-        });
+          return output.unshift(value);
+        }).then(nextInput);
       });
     };
 
     nextInput();
-  }
-
-  function next(value?: TInput) {
-    if (value != null) {
-      run();
-      inputQueue.unshift(value);
-    } else if (!keepOpen) {
-      Promise.resolve().then(() => {
-        if (!_pumping) {
-          outputQueue.return();
-        }
-      });
-    }
-
-    return outputQueue.next();
-  }
-
-  return {
-    [Symbol.asyncIterator]: () => this,
-    next: next
-  };
+  });
 }
