1d3684054538557009f7db8f8b5601dd006d5c94
Craig Condon <craig.j.condon@gmail.com>
2017-07-11 12:51:43 -0400

    build docs

--- packages/mesh/scripts/build-docs.js
+++ packages/mesh/scripts/build-docs.js
@@ -7,12 +7,12 @@ const repoVersion = require('../../../package').version;
 const BASE_PATH = join(__dirname, '..');
 
 function postprocess(md) {
-  return md.replace(/\[&#x24C8;\]\((.*?) \"View in source"\) \[&#x24C9;\]\[1\]/g, "[View in source]($1)");
+  return md.replace(/\[&#x24C8;\]\((.*?) \"View in source"\) \[&#x24C9;\]\[1\]/g, "\n[View in source]($1)");
 }
 
 const md = postprocess(docdown({
   'path': join(BASE_PATH, 'lib', 'bundle.js'),
-  'title': `<a href="https://mesh.js.com/">Mesh</a> <span>v${ repoVersion }</span>`,
+  'title': `<a href="https://mesh.js.com/">Mesh</a> <span>v${ version }</span>`,
   'toc': 'categories',
   'url': `https://github.com/crcn/mesh.js/blob/${ repoVersion }/packages/mesh/lib/bundle.js`
 }));
