155ea77ba3bf8ae55b7579f4d9ca5bad81d828f1
Craig Condon <craig.j.condon@gmail.com>
2017-07-24 10:09:19 -0400

    args more forgiving

--- packages/mesh/src/circular.ts
+++ packages/mesh/src/circular.ts
@@ -1,7 +1,7 @@
 import { proxy } from "./proxy";
 import { createDeferredPromise } from "./deferred-promise";
 
-export const circular = <TTarget extends (...args) => any>(create: (upstream: Function) => (downstream: Function) => TTarget) => (downstream: Function): TTarget => {
+export const circular = <TTarget extends (...args) => any>(create: (upstream: (...args) => any) => (downstream: (...args) => any) => TTarget) => (downstream: (...args) => any): TTarget => {
   const { promise: upstreamPromise, resolve: resolveUpstreamDispatcher } = createDeferredPromise<TTarget>();
   const top = create(proxy(() => upstreamPromise) as any)(downstream);
   resolveUpstreamDispatcher(top);
