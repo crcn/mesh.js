2b4b7777e83dad6148b0fce79cb0db04bce5689a
Craig Condon <craig.j.condon@gmail.com>
2017-07-28 09:14:24 -0400

    add Iterable type

--- packages/mesh/src/read-all.ts
+++ packages/mesh/src/read-all.ts
@@ -3,6 +3,7 @@ import { pump } from "./pump";
 
 export function readAll<T>(stream: DuplexAsyncIterableIterator<any, T>): Promise<T[]>;
 export function readAll<T>(stream: AsyncIterableIterator<T>): Promise<T[]>;
+export function readAll<T>(stream: IterableIterator<T>): Promise<T[]>;
 
 export function readAll<T>(source: any): Promise<T[]> {
   return new Promise((resolve, reject) => {
